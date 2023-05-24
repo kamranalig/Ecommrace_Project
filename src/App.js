16055e8fe8d6bd53b4fa4bb4a1c5460c2ac37294
Kamran Ali <km775.ali@gmail.com>
2023-05-24 10:37:58 +0500

    Make All Pages component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import AppNavbar from './commonComponent/Navbar/Navbar';
 import HomePage from './Pages/landing page/HomePage';
+import AllProducts from "./Pages/products/AllProducts";
+import BlogsPage from "./Pages/blogs/BlogsPage";
+import AboutPage from  "./Pages/about/AboutPage"
+import ContactPage from "./Pages/contact/ContactPage";
+import LoginPage from "./Pages/auth/LoginPage";
+import AddToCartPage from "./Pages/addToCart/AddToCartPage";
 import Footer from './commonComponent/Footer/Footer';
 
 function App() {
   return (
     <>
       <AppNavbar />
-      <HomePage/>
+       <HomePage/>
+       <AllProducts/>
+       <BlogsPage/>
+       <AboutPage/>
+       <ContactPage/>
+       <LoginPage/>
+       <AddToCartPage/>
       <Footer/>
     </>
   );
